b6c111b858e397e97183bc9769f76ef7633ef567
sebas <sborja@softcaribbean.com>
2022-11-29 16:24:07 -0500

    Pruebas Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'practica-angular';
+  name = 'Sebastian';
+  age = 18;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled = true;
+
+  person = {
+    name:'Sebas',
+    age:19,
+    img:'https://www.w3schools.com/howto/img_avatar.png'
+  }
+
+  Desactivar(){
+    this.btnDisabled =! this.btnDisabled;
+  }
+
+  Edad(){
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  cambiarNombre(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
---
+++ src/app/recap.ts
@@ -0,0 +1,10 @@
+const username: string = 'sebas';
+const sum = (a:number, b:number) =>{
+  return a + b;
+}
+
+class Person{
+  constructor(age:number, lastName:string){}
+}
+
+const sebas = new Person(19,'borja');
